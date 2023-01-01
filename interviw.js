d51bd418d31084ed27df49f010b4146e1bd7a7b6
Rituchoudhary67 <114747083+Rituchoudhary67@users.noreply.github.com>
2023-01-01 23:26:30 +0530

    Add files via upload

---
+++ interviw.js
@@ -0,0 +1,15 @@
+// js question for interview
+
+1. what is js?
+2. what are arrow function is js?
+3. DOM in js?
+4. js is single thread or multi-thread?
+5. call ,bind ,apply difference?
+6. closer in js?
+7. prototype chain ?
+8. hosting in js?
+9. first order function?
+10.difference between let, var, const?
+11.web storage? 
+12.events?
+13.loops syntax? 
